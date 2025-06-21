511e39a41f4b30334b9625b12af0969d3dd28136
martin <goudingcheng@gmail.com>
2025-06-21 22:36:57 +0800

    bugfix: fix some error

--- src/main/java/com/github/paohaijiao/anno/JSONField.java
+++ src/main/java/com/github/paohaijiao/anno/JSONField.java
@@ -8,6 +8,8 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface JSONField {
-    String name() default ""; // 反序列化字段名
-    String format() default ""; // 日期格式
+
+    String name() default "";
+
+    String format() default "";
 }

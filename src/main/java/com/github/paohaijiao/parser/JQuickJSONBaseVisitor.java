3672e0a147068cc99b007735a6c6dfd5d4f8ec73
martin <goudingcheng@gmail.com>
2025-06-24 19:16:10 +0800

    feature:update json lexer

--- src/main/java/com/github/paohaijiao/parser/JQuickJSONBaseVisitor.java
+++ src/main/java/com/github/paohaijiao/parser/JQuickJSONBaseVisitor.java
@@ -1,4 +1,4 @@
-// Generated from D:/my/jquick-curl/jthornruleGrammer/json/JQuickJSON.g4 by ANTLR 4.13.2
+// Generated from D:/idea/jthornruleGrammer/json/JQuickJSON.g4 by ANTLR 4.13.2
 package com.github.paohaijiao.parser;
 import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
 

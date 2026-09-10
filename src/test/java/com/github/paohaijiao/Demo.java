84be4263bb7b3786a8ff1b948d11a9a60537ebb8
martin <goudingcheng@gmail.com>
2026-09-11 06:50:25 +0800

    feat: upgrade the version to 1.5.0

---
+++ src/test/java/com/github/paohaijiao/Demo.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) [2025-2099] Martin (goudingcheng@gmail.com)
+ */
+package com.github.paohaijiao;
+
+import com.github.paohaijiao.factory.JSONSerializerFactory;
+import com.github.paohaijiao.serializer.JSONSerializer;
+
+public class Demo {
+
+    public static class User {
+        private String name;
+        private Integer age;
+
+        public User() {
+        }
+
+        public User(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "User{name='" + name + "', age=" + age + "}";
+        }
+    }
+
+    public static void main(String[] args) {
+        JSONSerializer serializer = JSONSerializerFactory.getDefaultSerializer();
+
+        User user = new User("John", 30);
+        String json = serializer.serialize(user);
+        System.out.println("serialize: " + json);
+
+        User result = serializer.deserialize(json, User.class);
+        System.out.println("deserialize: " + result);
+    }
+}
+
